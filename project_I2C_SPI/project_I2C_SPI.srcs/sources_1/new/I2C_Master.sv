3f32ad52412c66f30676dda301796b5df700e45b
pumpk <cong2738@gmail.com>
2025-05-23 15:50:34 +0900

    I2C_Master 모듈: master_mode_next 신호 추가 및 초기화 수정

--- project_I2C_SPI/project_I2C_SPI.srcs/sources_1/new/I2C_Master.sv
+++ project_I2C_SPI/project_I2C_SPI.srcs/sources_1/new/I2C_Master.sv
@@ -46,7 +46,7 @@ module I2C_Master (
     logic [7:0] temp_tx_data, temp_tx_data_next;
     logic [7:0] temp_rx_data, temp_rx_data_next;
     logic sda_IO, sda_IO_next;
-    logic master_mode;
+    logic master_mode, master_mode_next;
     logic sda_reg;
 
     assign sda = sda_IO ? 1'bz : sda_reg;
@@ -60,6 +60,7 @@ module I2C_Master (
             temp_tx_data <= 0;
             temp_rx_data <= 0;
             sda_IO       <= 0;
+            master_mode  <= 0;
         end else begin
             state        <= next;
             clk_count    <= clk_count_next;
@@ -67,6 +68,7 @@ module I2C_Master (
             temp_tx_data <= temp_tx_data_next;
             temp_rx_data <= temp_rx_data_next;
             sda_IO       <= sda_IO_next;
+            master_mode  <= master_mode_next;
         end
     end
     always_comb begin : next_logic
@@ -76,7 +78,7 @@ module I2C_Master (
         temp_tx_data_next = temp_tx_data;
         temp_rx_data_next = temp_rx_data;
         sda_IO_next = sda_IO;
-        master_mode = 0;
+        master_mode_next = master_mode;
         tx_done = 0;
         rx_done = 0;
         sda_reg = 1;
@@ -118,6 +120,7 @@ module I2C_Master (
                 sda_reg = 0;
                 scl = 0;
                 temp_tx_data_next = tx_data;
+                master_mode_next = 0;
                 case (CMD)
                     STOP_CMD: begin
                         sda_IO_next = 0;
@@ -133,7 +136,7 @@ module I2C_Master (
                         next = DATA1;
                     end
                     RD_CMD: begin
-                        master_mode = 1;
+                        master_mode_next = 1;
                         sda_IO_next = 1;
                         next = DATA1;
                     end
